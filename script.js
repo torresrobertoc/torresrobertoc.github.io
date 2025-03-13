ae615d9a4331214e287e5b2e05daac55f70a2b91
Roberto Torres <torresrobertoc@hotmail.com>
2025-03-13 08:19:19 -0600

    Writings page + a couple post pages.

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    loadComponent("component_navbar.html", "#navbar");
+    loadComponent("component_footer.html", "#footer");
+});
+
+function loadComponent(url, selector) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => document.querySelector(selector).innerHTML = data)
+        .catch(error => console.error(`Error loading ${url}:`, error));
+}
+
 // Toggle Mobile Menu
 function toggleMenu() {
     var navLinks = document.querySelector(".nav-links");
     navLinks.classList.toggle("show");
 }
 
-
+// Show navbar on scroll up
 let lastScroll = 0;
 window.addEventListener("scroll", () => {
     let currentScroll = window.pageYOffset;
